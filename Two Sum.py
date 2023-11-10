0b527f313594bbb862d04279763c4a15a3447198
Meng Xue <xuemon@gmail.com>
2023-11-10 10:52:30 -0500

    Create Two Sum.py

---
+++ Two Sum.py
@@ -0,0 +1,25 @@
+############### Slow ###############
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j and nums[i] + nums[j] == target:
+                        return [i, j]
+        return []
+
+  ############### Fast ###############
+  class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+
+        for index in range(len(nums)):
+            val = nums[index]
+            complement = target - val
+
+            # Do not need to consider if they are the same element, because it's impossible
+            if complement in dic:
+                return [index, dic[complement]]
+            
+            dic[val] = index
+
+        return []
