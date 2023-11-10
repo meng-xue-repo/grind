de3f5117536b40fa46101692f08203144e80ac1f
Meng Xue <xuemon@gmail.com>
2023-11-10 12:22:34 -0500

    Create  Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,38 @@
+########### A bit slow ###########
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        for c in s:
+            stack.append(c)
+
+            if len(stack) > 1:
+                if (stack[-2] == '(' and c == ')') or (stack[-2] == '{' and c == '}') or (stack[-2] == '[' and c == ']'):
+                    stack.pop()
+                    stack.pop()
+
+        return len(stack)  == 0
+
+
+########### A bit faster ###########
+from collections import deque
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        for c in s:
+            if c in "({[":
+                stack.append(c)
+            elif len(stack) == 0:
+                return False # Do not continue
+            elif stack[-1] == '(' and c != ')':
+                return False # Do not continue
+            elif stack[-1] == '{' and c != '}':
+                return False # Do not continue
+            elif stack[-1] == '[' and c != ']':
+                return False # Do not continue
+            else:
+                stack.pop()
+
+        return not stack
