d93dee052ce789afff5be72f913080e8d0979b0f
Meng Xue <xuemon@gmail.com>
2023-11-10 19:57:34 -0500

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i != j and nums[i] + nums[j] == target:
-                        return [i, j]
+                    return [i, j]
         return []
 
 
