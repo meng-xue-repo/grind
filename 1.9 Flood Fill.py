9f3a23388513594c073db6d4fbeee25acad5adf6
Meng Xue <xuemon@gmail.com>
2023-11-11 19:29:24 -0500

    Create 1.9 Flood Fill.py

---
+++ 1.9 Flood Fill.py
@@ -0,0 +1,33 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+        visited = set() # "{}" means dict, NOT set
+
+        # Have to fix the original color at that position!
+        originalColor = image[sr][sc]
+
+        def inBoundary(row, col):
+            return row >= 0 and row < len(image) and col >= 0 and col < len(image[row])
+
+        def traversal(row, col):
+            if not inBoundary(row, col):
+                return
+
+            if (row, col) in visited:
+                return
+
+            if image[row][col] != originalColor:
+                return
+
+            image[row][col] = color
+            visited.add((row, col)) # NOT append()
+
+            traversal(row - 1, col)
+            traversal(row + 1, col)
+            traversal(row, col - 1)
+            traversal(row, col + 1)
+
+        traversal(sr, sc)
+
+        return image      
+                
