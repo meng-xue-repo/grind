823cdaa0619116cddf4dce89fd1a58c1e8f42c0c
Meng Xue <xuemon@gmail.com>
2023-11-12 11:23:38 -0500

    Update 1.10 Lowest Common Ancestor of a Binary Search Tree.py

--- 1.10 Lowest Common Ancestor of a Binary Search Tree.py
+++ 1.10 Lowest Common Ancestor of a Binary Search Tree.py
@@ -5,6 +5,7 @@
 #         self.left = None
 #         self.right = None
 
+################### SLOW solution, because i didn't notice it's a BINARY SEARCH tree !!! ###################
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def hasNode(node, n):
@@ -36,4 +37,24 @@ class Solution:
             return self.lowestCommonAncestor(root.left, p, q)
 
         if rightHasP: # in this case, rightHasQ == True
+            return self.lowestCommonAncestor(root.right, p, q)
+
+
+################### CORRECT solution benefits from Binary search tree ###################
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        if root.val == p.val or root.val == q.val:
+            return root
+        
+        if p.val < root.val < q.val:
+            return root
+
+        if q.val < root.val < p.val:
+            return root
+
+        if p.val < root.val: # in this case, q.val < root.val
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        if root.val < p.val: # in this case, root.val < q.val
             return self.lowestCommonAncestor(root.right, p, q)
