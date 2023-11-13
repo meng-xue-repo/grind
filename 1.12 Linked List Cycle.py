75d8f695ebe0d3fb530366844900a8349a8d89c2
Meng Xue <xuemon@gmail.com>
2023-11-12 21:16:26 -0500

    Create 1.12 Linked List Cycle.py

---
+++ 1.12 Linked List Cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+'''
+According to ChatGpt, if a class does not define custom __hash__ and __eq__ methods, the default hash value and equality check is based on object's IDENTITY (Memory Address).
+When you want to consider objects equal based on their CONTENT rather than their identity, it's recommended to define custom __hash__ and __eq__
+'''
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        node = head
+
+        while node and node not in visited:
+            visited.add(node)
+            node = node.next
+
+        return node in visited
